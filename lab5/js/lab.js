014867e429c4c1f3c316b4e8ed10f8cccc6a9985
Chloe Seifert <czseifer@ucsc.edu>
2021-01-27 18:40:43 -0800

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,17 @@
+//define skateboard variables
+const make = Jeep;
+const model = Gladiator;
+const color = yellow;
+const year = 2021;
+const ownIt = true;
+const age = 2021 - year;
+
+//writes in document
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+
+
+document.writeln("Age: " + age + "<br>");
+console.log("test");
