0cd0aa556aa75968253744e335d634da934ba59d
Chloe Seifert <czseifer@ucsc.edu>
2021-02-07 18:27:14 -0800

    Update lab8.js

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -1,17 +1,17 @@
 //create three functions that take a parameter test
 //test 1 -- call functions in order
 function firstThing(test) {
-  console.log(test + ": First Thing <br>")
+  document.writeln(test + ": First Thing <br>");
 }
 function secondThing(test) {
-  console.log(test + ": Second Thing <br>")
+  document.writeln(test + ": Second Thing <br>");
 }
 function thirdThing(test) {
-  console.log(test + ": Third Thing <br>")
+  document.writeln(test + ": Third Thing <br>");
 }
 firstThing("Test 1");
 secondThing("Test 1");
-thirdThing("test 1");
+thirdThing("Test 1");
 //test 2 -- call functions in order
 setTimeout(function() {
   firstThing("Test 2");
